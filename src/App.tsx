eee346f27562ebf49c3f3c32574c115ce8feed37
Moustafa Essam <moustafaessam2212@gmail.com>
2025-03-29 15:01:50 +0800

    first commit with global styling added

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { GlobalStyle } from "./GlobalStyle";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+}
---
+++ src/GlobalStyle.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * , ::before , ::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  :root {
+    // Colors
+    --grey-900: #2A4144;
+    --grey-500: #86A2A5;
+    --white: #FFFFFF;
+    --green-600:#0C7D69;
+    --green-200: #E0F1E8; 
+    --red: #D73C3C; 
+    // Font size
+    --heading: 3.2rem;
+    --body-md: 1.8rem;  
+    --body-sm: 1.6rem;
+    // Spacing
+    --100: 0.8rem;
+    --150: 1.2rem;
+    --200: 1.6rem;
+    --300: 2.4rem;
+    --400: 3.2rem;
+    --500: 4rem;
+    --1600: 12.6rem;       
+  }
+  body {
+    background-color: var(--green-200);
+  }
+`;
